cc8537d74df436445a5c68c9281b6116e440ea94
selvapriyasuhana <142295945+selvapriyasuhana@users.noreply.github.com>
2024-01-02 13:07:50 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <>
+    {/* Variable Example */}
+    <h1>This is a variable example</h1>
+   
+   {/* Nested Rules */}
+   <div id="nested">
+      <h3>This is a nested rules example</h3>
+   </div>
+
+   {/* Interpolation Example */}
+     <p className="interpolation">Interpolation Example.</p>
+
+     {/* Function Example */}
+     <div id="function">
+     <h3>This is a function example</h3>
+     </div>
+
+     {/* Mixin Example */}
+    <div id="mixin">
+    <p>This is mixin Example</p>
+    </div>
+  </>
+   
+  );
+}
+
+export default App;
